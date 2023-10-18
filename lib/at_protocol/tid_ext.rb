d4cf0b05d6427476da6a2286371830d7b695b550
Shreyan Jain <shreyan.jain.9@outlook.com>
2023-10-18 11:01:02 -0700

    Add ATProto::TID.str?
    In case statements, you can do:
    
    ```ruby
    case "3kc275kpkay2y"
    when ATProto::TID.str? 
       puts "Valid TID!"
    else 
       puts "Not a TID :("
    end #=> "Valid TID!"
    ```

--- lib/at_protocol/tid_ext.rb
+++ lib/at_protocol/tid_ext.rb
@@ -17,3 +17,15 @@ class ATProto::TID
     "#<ATProto::TID(#{self.to_s})>"
   end
 end
+
+
+class << ATProto::TID
+  # For use in case statements, so you can see if a string is a valid TID
+  def str?
+    o = Object.new
+    def o.===(str)
+        ATProto::TID.from_string(str).to_s == str
+    end
+    o
+  end
+end
