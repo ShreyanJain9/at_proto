e553bbada5897fc4412f157367834e8419bba005
Shreyan Jain <shreyan.jain.9@outlook.com>
2023-11-15 14:32:56 -0800

    one-liners

--- lib/at_protocol/record/strongref.rb
+++ lib/at_protocol/record/strongref.rb
@@ -7,16 +7,12 @@ module ATProto
       const :uri, ATProto::AtUri
       prop :cid, Skyfall::CID
 
-      def to_json
-        {
+      def to_json = {
           "uri" => uri.to_s,
           "cid" => cid.to_s,
         }
-      end
 
-      def inspect
-        self.to_json.to_json
-      end
+      def inspect = JSON(self.to_json)
     end
   end
 end
